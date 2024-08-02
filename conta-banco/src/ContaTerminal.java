fb8129ff2942fca2080a2e11d0f202a9d26857cf
yagowill <yagowill3@gmail.com>
2024-08-02 18:50:50 -0300

    input do usuário e mensagem no final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Seja bem vindo, por favor, preencha os dados solicitados abaixo:");
+        System.out.print("Número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+                
+        System.out.print("Número da Agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.print("Nome e sobrenome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.print("Valor que você quer depositar: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("\nOlá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta "+ numeroConta +" e seu saldo é de R$ " + saldo + " e já está disponível para saque");
+        
+        scanner.close();
+
     }
 }
